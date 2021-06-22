48b2d10f4d8d261a8b41129fb64a2eddb3f581f0
川田 隆稀 <>
2021-06-22 13:17:28 +0900

    7.5 プロのデプロイ　の前まで終了

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+    def show
+        @user = User.find(1)
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        # @user = User.new(params[:user])    # 実装は終わっていないことに注意!
+        @user = User.new(user_params)
+        if @user.save
+            # 保存の成功をここで扱う。
+            flash[:success] = "Welcome!!!!!!!!!!!"
+            redirect_to user_url(@user)
+        else
+            render 'new'
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password,
+                                     :password_confirmation)
+    end
+end
+
