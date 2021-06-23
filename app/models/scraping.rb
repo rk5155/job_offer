24b832b15c8fbb1ac06bec2b730fe59651fd0f3e
川田 隆稀 <>
2021-06-23 18:00:45 +0900

    求人一覧から、格詳細ページに移動してタイトルをデータベースに保存できるようにした

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -3,13 +3,20 @@
 class Scraping < ApplicationRecord
     # rails c で Scraping.scrape を実行すればScrapingデータベースに保存できる
     def self.scrape
-        agent = Mechanize.new                  #agentは任意の変数
-        page = agent.get("https://tenshoku.mynavi.jp/list/")  #getの引数はサイトのURL
-        elements = page.search('h3') #取得したい要素
+        agent = Mechanize.new
+        #getの引数はサイトのURL
+        page = agent.get("https://tenshoku.mynavi.jp/list/")
+        
 
-        elements.each do |element|
-          scraping = Scraping.new                      #Fishは任意のクラス、fishは任意のインスタンス
-          scraping.name = element.inner_text
+        page.css(".cassetteRecruit__bottom a").each do |ele|
+          url = ele[:href]
+          # 一覧ページから、格詳細ページへ移動
+          details_page = page.link_with(href: url).click
+          #取得したい要素
+          title = details_page.search('.occName')
+          # Scrapingテーブルへ保存
+          scraping = Scraping.new
+          scraping.name = title.inner_text
           scraping.save
         end
     end
