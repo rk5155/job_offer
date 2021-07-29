ef9f18020f1a0b524d2aff5e0f79f073ce99c62a
rk <>
2021-07-29 18:07:12 +0900

    企業、ユーザーがログインした時、それぞれのマイページへ遷移するようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,17 @@ class ApplicationController < ActionController::Base
 
   # ログイン後に遷移するページ
   def after_sign_in_path_for(resource)
-    mypage_path(resource)
+    mypage_companies_path(resource)
+  end
+
+  def after_sign_in_path_for(resource)
+    case resource
+
+    when Company
+      mypage_companies_path(resource)
+
+    when UserDevise
+      mypage_user_devises_path(resource)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,11 @@ Rails.application.routes.draw do
   devise_for :companies
   get  "companies/applies"  => "companies#applies"
   resources :companies, only: [:show]
-  get "companies/:id" => "companies#show", as: :mypage
+  get "companies/:id" => "companies#show", as: :mypage_companies
 
   devise_for :user_devises
   resources :user_devises, only: [:show]
+  get "user_devises/:id" => "user_devises#show", as: :mypage_user_devises
 
   root 'scrapings#index'
   get  "scrapings/search"  => "scrapings#search"
