78cdec4f0a43cc1e68e05bd898f1fadca37904ed
rk <>
2021-08-11 14:51:55 +0900

    企業マイページのレイアウトを整えた

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,7 +1,7 @@
 class CompaniesController < ApplicationController
   def show
     @company = Company.find(params[:id])
-    
+    @job_list = Scraping.where(company_id: @company.id)
   end
 
   def applies
