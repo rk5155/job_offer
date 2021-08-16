603f9e4773c887d63dd372a4df9fb304deb85a69
rk <>
2021-08-17 08:31:00 +0900

    ユーザー情報のレイアウトを調整し、編集できるようにしたが、編集内容が反映されない

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,18 @@ class ApplicationController < ActionController::Base
       mypage_user_devises_path(resource)
     end
   end
+
+
+
+
+  # deviseコントローラーにストロングパラメータを追加する          
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    # サインアップ時にnameのストロングパラメータを追加
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    # アカウント編集の時にnameとprofileのストロングパラメータを追加
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
